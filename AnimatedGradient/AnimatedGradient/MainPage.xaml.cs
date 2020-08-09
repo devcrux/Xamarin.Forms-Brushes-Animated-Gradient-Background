c20cc72f5a9163b6dfac88407bde913965d9b1e8
Oludayo Alli <sunnymexy@gmail.com>
2020-08-09 21:19:22 +0100

    Add project files.

---
+++ AnimatedGradient/AnimatedGradient/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimatedGradient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Task.Run(AnimateBackground);
+        }
+
+        private async void AnimateBackground()
+        {
+            Action<double> forward = input => bdGradient.AnchorY = input;
+            Action<double> backward = input => bdGradient.AnchorY = input;
+
+            while (true)
+            {
+                bdGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+                bdGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
